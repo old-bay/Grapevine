febb1379d60f81d5e87f8770e435440df428afc3
Old Bay <lancastermikael1994@gmail.com>
2024-08-24 22:47:24 -0400

    test commit for pull from other machine

--- 01-Basics/myObjects.ts
+++ 01-Basics/myObjects.ts
@@ -1,14 +1,59 @@
+
+/*
 const User = {
     name: "Old Bay",
     email: "crab@oldbay.moe",
     isActive: true;
 }
 
-function createUser({name: string, isPaid: boolean}) {}
+//function createUser({name: string, isPaid: boolean}) {}
 createUser({name: "Old Bay", isPaid: false});
 
 function createCourse(): {name: string, price: number} {
     return {name: "Old Bay", price: 3}
 }
+
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+
+function createUser(user: User): User {
+    return {name: "", email: "", isActive: false}
+}
+
+createUser({name: "", email: "", isActive: false});
+*/
+
+type User = {
+    readonly _id: string;
+    name: string;
+    email: string;
+    isActive: boolean;
+    creditCardDetails?: number;
+}
+
+let myUser: User = {
+    _id: "12345",
+    name: "h",
+    email: "h@h.com",
+    isActive: false
+}
+
+type cardNumber = {
+    cardNumber: string;
+}
+
+type cardDate = {
+    cardDate: string;
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number;
+}
+
+myUser.email = "crab@oldbay.moe";
 
 export {}
