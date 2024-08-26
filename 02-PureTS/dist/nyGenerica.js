947e4ea66ce66fcf087bc3bf54f767e9eb6f4932
Old Bay <lancastermikael1994@gmail.com>
2024-08-25 21:29:59 -0400

    made some edits

--- 02-PureTS/dist/nyGenerica.js
+++ 02-PureTS/dist/nyGenerica.js
@@ -8,3 +8,36 @@ function identityOne(val) {
 function identityTwo(val) {
     return val;
 }
+function identityThree(val) {
+    return val;
+}
+let val = identityThree(3);
+// simplified syntax
+function identityFour(val) {
+    return val;
+}
+// let value = identityFour({})
+// example of array return generic
+function getSearchProduct(products) {
+    const myIndex = 3;
+    return products[myIndex];
+}
+// example as an arrow function
+const getMoreSearchProduct = (products) => {
+    const myIndex = 4;
+    return products[myIndex];
+};
+function aFunction(val1, val2) {
+    return {
+        val1,
+        val2
+    };
+}
+class Sellable {
+    constructor() {
+        this.cart = [];
+    }
+    addToCart(product) {
+        this.cart.push(product);
+    }
+}
--- 02-PureTS/src/nyGenerica.ts
+++ 02-PureTS/src/nyGenerica.ts
@@ -9,6 +9,67 @@ function identityTwo(val: any): any {
     return val;
 }
 
+function identityThree<Type>(val: Type): Type {
+    return val;
+}
 
+let val: number = identityThree(3);
+
+// simplified syntax
+function identityFour<T>(val: T): T {
+    return val;
+}
+
+interface Bottle {
+    brand: string,
+    type: number
+}
+
+// let value = identityFour({})
+
+// example of array return generic
+function getSearchProduct<T>(products: T[]): T {
+    const myIndex = 3;
+    return products[myIndex];
+}
+
+// example as an arrow function
+const getMoreSearchProduct = <T>(products: T[]): T => {
+    const myIndex = 4;
+    return products[myIndex];
+}
+
+interface Database {
+    connection: string,
+    username: string,
+    password: string
+}
+
+function aFunction<T, U extends Database>(val1: T, val2: U): obj {
+    return {
+        val1,
+        val2
+    }
+}
+
+// const values = aFunction(3, "4");
+
+interface Quiz {
+    name: string,
+    type: string
+}
+
+interface Course {
+    name: string,
+    author: string,
+    subject: string
+}
+
+class Sellable<T> {
+    public cart: T[] = [];
+    addToCart(product: T) {
+        this.cart.push(product);
+    }
+}
 
 export {}
