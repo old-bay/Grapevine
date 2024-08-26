68ff155218991cd54135da0e9def5182ef477013
Old Bay <lancastermikael1994@gmail.com>
2024-08-25 21:33:59 -0400

    test

--- 02-PureTS/dist/second.js
+++ 02-PureTS/dist/second.js
@@ -1,18 +1,23 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Insta {
-    constructor(cameraMode, filter, burst) {
-        this.cameraMode = cameraMode;
-        this.filter = filter;
-        this.burst = burst;
-    }
-}
-// interface implementations must be implemented at the minimum
-class UTube {
-    constructor(cameraMode, filter, burst, mini) {
-        this.cameraMode = cameraMode;
-        this.filter = filter;
-        this.burst = burst;
-        this.mini = mini;
-    }
-}
+// interface TakePhoto {
+//     cameraMode: string;
+//     filter: string;
+//     burst: number;
+// }
+// class Insta implements TakePhoto {
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst:number
+//     ) {}
+// }
+// // interface implementations must be implemented at the minimum
+// class UTube implements TakePhoto {
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst:number,
+//         public mini: string
+//     ) {}
+// }
+// export {}
--- 02-PureTS/src/second.ts
+++ 02-PureTS/src/second.ts
@@ -1,25 +1,25 @@
-interface TakePhoto {
-    cameraMode: string;
-    filter: string;
-    burst: number;
-}
+// interface TakePhoto {
+//     cameraMode: string;
+//     filter: string;
+//     burst: number;
+// }
 
-class Insta implements TakePhoto {
-    constructor(
-        public cameraMode: string,
-        public filter: string,
-        public burst:number
-    ) {}
-}
+// class Insta implements TakePhoto {
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst:number
+//     ) {}
+// }
 
-// interface implementations must be implemented at the minimum
-class UTube implements TakePhoto {
-    constructor(
-        public cameraMode: string,
-        public filter: string,
-        public burst:number,
-        public mini: string
-    ) {}
-}
+// // interface implementations must be implemented at the minimum
+// class UTube implements TakePhoto {
+//     constructor(
+//         public cameraMode: string,
+//         public filter: string,
+//         public burst:number,
+//         public mini: string
+//     ) {}
+// }
 
-export {}
+// export {}
